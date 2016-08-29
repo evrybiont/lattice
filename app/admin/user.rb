6195565e9840d1def86e1491d184affa9dacec5b
evrybiont <evrybiont@gmail.com>
2016-08-29 13:50:51 +0300

    Add custom user login

---
+++ app/admin/user.rb
@@ -0,0 +1,11 @@
+ActiveAdmin.register User do
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :sign_in_count
+    column :last_sign_in_at
+    column :created_at
+    actions
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
   resources :products
+
   root 'products#index'
+
+  get '*unmatched_route', to: 'application#not_found'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
